4c3739a1c3c8ae7837c86c8fe96050b380f42e22
i.aryka <i.aryka@hackspace.by>
2019-03-04 14:13:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.testapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.randomuserfeature.fragment.UsersScreen
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                //message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                //message.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.container, UsersScreen())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
