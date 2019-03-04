c9e4d506f4eab04fd57ed3b13687be63b2bc4094
i.aryka <i.aryka@hackspace.by>
2019-03-04 14:17:55 +0300

    [REF] add api interface

---
+++ randomuserfeature/src/main/java/com/example/randomuserfeature/api/RandomUsersApi.kt
@@ -0,0 +1,16 @@
+package com.example.randomuserfeature.api
+
+import com.example.randomuserfeature.api.entities.Users
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Created by Hari on 20/11/17.
+ */
+
+interface RandomUsersApi {
+
+    @GET("api/")
+    fun getRandomUsers(@Query("results") size: Int): Single<Users>
+}
