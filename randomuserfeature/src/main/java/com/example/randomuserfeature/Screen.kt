d6c8be8ca7041bd8eedd9c7a2ba87a822250a8f1
i.aryka <i.aryka@hackspace.by>
2019-03-04 14:16:13 +0300

    [REF] add base pm components

---
+++ randomuserfeature/src/main/java/com/example/randomuserfeature/Screen.kt
@@ -0,0 +1,48 @@
+package com.example.randomuserfeature
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import me.dmdev.rxpm.base.PmSupportFragment
+
+
+abstract class Screen<PM : ScreenPresentationModel> :
+    PmSupportFragment<PM>(),
+    BackHandler {
+
+    abstract val screenLayout: Int
+
+    final override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(screenLayout, container, false)
+    }
+
+    override fun onBindPresentationModel(pm: PM) {
+        pm.errorDialog bindTo { message, _ ->
+            AlertDialog.Builder(context!!)
+                .setMessage(message)
+                .setPositiveButton("Ok", null)
+                .create()
+        }
+    }
+
+    override fun handleBack(): Boolean {
+        presentationModel.backAction.consumer.accept(Unit)
+        return true
+    }
+
+//    val progressConsumer = Consumer<Boolean> {
+//        if (it) {
+//            childFragmentManager.showDialog(ProgressDialog())
+//        } else {
+//            childFragmentManager
+//                .findScreen<ProgressDialog>()
+//                ?.dismiss()
+//        }
+//    }
+}
