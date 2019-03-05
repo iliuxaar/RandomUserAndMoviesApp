cf45d27312f5d308f573784c64e8b7a0dc817961
i.aryka <i.aryka@hackspace.by>
2019-03-05 21:56:48 +0300

    [REF] add class for providing resources

---
+++ randomuserfeature/src/main/java/com/example/randomuserfeature/utils/ResourceProvider.kt
@@ -0,0 +1,13 @@
+package com.example.randomuserfeature.utils
+
+import android.content.Context
+import androidx.annotation.StringRes
+
+class ResourceProvider(private val context: Context){
+
+    fun getString(@StringRes stringId: Int) = context.resources.getString(stringId)
+
+    fun getString(@StringRes stringId: Int, vararg formatArgs: Any) = context.resources.getString(stringId, *formatArgs)
+
+    //TODO: there can be placed another resource getters, like getColor, getDimens, getDrawable etc.
+}
