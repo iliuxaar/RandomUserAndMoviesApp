c401108ac7088e032a0bf1d34da90b98d4629585
i.aryka <i.aryka@hackspace.by>
2019-03-05 21:57:26 +0300

    [REF] add second screen

--- randomuserfeature/src/main/java/com/example/randomuserfeature/fragment/UserDetailsScreen.kt
+++ randomuserfeature/src/main/java/com/example/randomuserfeature/fragment/UserDetailsScreen.kt
@@ -1,12 +1,35 @@
 package com.example.randomuserfeature.fragment
 
+import android.os.Bundle
+import android.util.Log
 import com.example.randomuserfeature.R
 import com.example.randomuserfeature.Screen
 import com.example.randomuserfeature.presentationmodel.UserDetailsPresentationModel
+import kotlinx.android.synthetic.main.user_detail_fragment_layout.*
 
 class UserDetailsScreen: Screen<UserDetailsPresentationModel>() {
 
+    companion object {
+        private const val ID = "valueId"
+        fun createInstance(valueId: String) = UserDetailsScreen().apply {
+            arguments = Bundle().apply { putString(ID, valueId) }
+            Log.d("test", "id is $valueId")
+        }
+    }
+
     override val screenLayout = R.layout.user_detail_fragment_layout
 
-    override fun providePresentationModel(): UserDetailsPresentationModel = UserDetailsPresentationModel()
+    override fun providePresentationModel(): UserDetailsPresentationModel {
+        return UserDetailsPresentationModel(
+            arguments!!.getString(ID)!!
+        )
+    }
+
+    override fun onBindPresentationModel(pm: UserDetailsPresentationModel) {
+        super.onBindPresentationModel(pm)
+
+        pm.userResult bindTo {
+            textView2.text = it.name.first
+        }
+    }
 }
--- randomuserfeature/src/main/java/com/example/randomuserfeature/presentationmodel/UserDetailsPresentationModel.kt
+++ randomuserfeature/src/main/java/com/example/randomuserfeature/presentationmodel/UserDetailsPresentationModel.kt
@@ -1,6 +1,40 @@
 package com.example.randomuserfeature.presentationmodel
 
+import android.util.Log
 import com.example.randomuserfeature.ScreenPresentationModel
+import com.example.randomuserfeature.api.entities.ResultsItem
+import com.example.randomuserfeature.di.RandomUserComponent
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
-class UserDetailsPresentationModel: ScreenPresentationModel() {
+class UserDetailsPresentationModel(
+    private val userId: String
+): ScreenPresentationModel() {
+
+    val userResult = State<ResultsItem>()
+
+    lateinit var loadTask: Disposable
+
+    override fun onCreate() {
+        super.onCreate()
+        loadUser()
+    }
+
+    private fun loadUser(){
+        loadTask = RandomUserComponent.randomUserApi.getUserById(userId)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe (
+                { users ->
+                    userResult.consumer.accept(users.results!![0])
+                },
+                { Log.e("test", it.message)}
+            )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        loadTask.dispose()
+    }
 }
