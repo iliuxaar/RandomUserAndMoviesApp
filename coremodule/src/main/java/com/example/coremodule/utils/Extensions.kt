a1e0f603b9d8487c8027c81b2ffd9a8689ed2ed1
i.aryka <i.aryka@hackspace.by>
2019-03-10 18:38:15 +0300

    [DEL] remove extension

--- coremodule/src/main/java/com/example/coremodule/utils/Extensions.kt
+++ coremodule/src/main/java/com/example/coremodule/utils/Extensions.kt
@@ -4,9 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import com.example.coremodule.navigation.fragmentfactory.BaseFragmentFactory
 
 fun ViewGroup.inflate(@LayoutRes layoutId: Int): View {
     return LayoutInflater.from(this.context).inflate(layoutId, this, false)
@@ -14,9 +11,4 @@ fun ViewGroup.inflate(@LayoutRes layoutId: Int): View {
 
 fun View.setVisibility(isVisible: Boolean) {
     this.visibility = if (isVisible) View.VISIBLE else View.GONE
-}
-
-fun FragmentManager.addFactoryWithTransaction(baseFragmentFactory: BaseFragmentFactory): FragmentTransaction {
-    this.fragmentFactory = baseFragmentFactory
-    return this.beginTransaction()
 }
