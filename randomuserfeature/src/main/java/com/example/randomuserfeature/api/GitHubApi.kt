dc54db2a87f7b134350e925f3316188ffc1fa739
i.aryka <i.aryka@hackspace.by>
2019-03-12 15:23:39 +0300

    [ADD] github api

---
+++ randomuserfeature/src/main/java/com/example/randomuserfeature/api/GitHubApi.kt
@@ -0,0 +1,21 @@
+package com.example.randomuserfeature.api
+
+import com.example.randomuserfeature.api.entities.User
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GitHubApi {
+
+    @GET("/users")
+    fun getUsers(@Query("since") userId: Long, @Query("per_page") perPage: Int): Single<List<User>>
+
+    @GET("/user/{id}")
+    fun getUserById(@Path("id") userId: Long): Single<User>
+
+    companion object {
+        const val GITHUB_API_URL = "https://api.github.com/"
+    }
+
+}
