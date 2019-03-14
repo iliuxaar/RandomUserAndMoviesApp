c71c296004ea767c9a0eb8584c4f254ac87d757e
i.aryka <i.aryka@hackspace.by>
2019-03-14 12:35:52 +0300

    [REF] update UserDetailsScreen

--- randomuserfeature/src/main/java/com/example/randomuserfeature/fragment/UserDetailsScreen.kt
+++ randomuserfeature/src/main/java/com/example/randomuserfeature/fragment/UserDetailsScreen.kt
@@ -1,6 +1,8 @@
 package com.example.randomuserfeature.fragment
 
 import android.os.Bundle
+import android.view.View
+import com.bumptech.glide.Glide
 import com.example.coremodule.pm.Screen
 import com.example.randomuserfeature.R
 import com.example.randomuserfeature.RandomUsersFlowFragment
@@ -24,15 +26,33 @@ class UserDetailsScreen: Screen<UserDetailsPresentationModel>() {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolbar()
+    }
+
     override val screenLayout = R.layout.user_detail_fragment_layout
 
     override fun providePresentationModel() = userDetailsPresentationModel
 
     override fun onBindPresentationModel(pm: UserDetailsPresentationModel) {
         super.onBindPresentationModel(pm)
-
         pm.userResult bindTo {
             secondScreenTextView.text = it.login
+            Glide.with(context!!)
+                .load(it.avatarUrl)
+                .into(userImage)
+        }
+    }
+
+    private fun initToolbar(){
+        userToolbar.inflateMenu(R.menu.user_details_toolbar_menu)
+        userToolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.user_delete_icon -> { true }
+                R.id.user_edit_icon -> { true }
+                else -> false
+            }
         }
     }
 }
